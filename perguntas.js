d26829fd67a1b4202932b67b7adf104eb6685774
bioeusilveiracaroline <107115344+bioeusilveiracaroline@users.noreply.github.com>
2024-08-09 08:58:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+export const perguntas = [
+    {
+        enunciado: "O que é IA? ",
+        alternativas: [
+            {
+                texto: "Aplicações que executam tarefas complexas!",
+                afirmacao: [
+                "Certa, ela pode se comunicar com clientes online, jogar xadrez entre outras coisas!",
+                "Certa, porque ela é eficiente para muitas coisas, podemos trabalhar de forma online, jogar jogos diversos e ainda fazer compras online!",
+                ]
+            },
+            {
+                texto: "É um jogo!",
+                afirmacao: [
+                "Errada, porque não faz sentido ser um jogo e é algo muito complexo para ser um simples jogo!",
+                "Errada, porque a inteligência artificial vai muito além do que pensamos ela tem inúmeras funções para serem utilizdas!",
+                ]
+            }
+
+
+        ]
+    },
+    {
+        enunciado: "Quando iniciou o novo ensino médio nos deparamos com uma nova realidade, muita tecnologia inovadora nas novas matérias. Qual foi a sensação de estamos diante dessa situação? ",
+        alternativas: [
+            {
+                texto:  "Que desespero como vamos nos adaptar com tantas tecnologias!",
+                afirmacao: [
+                 "Errada, porque temos muita capacidade de aprender e não devemos ficar preocupados em aprender coisas novas!",
+                 "Errada, porque com o tempo nos acostumamos com a tecnologia e aprendemos muito com ela!",
+                ]
+            },
+            {
+                texto: "Isso é super interessante, finalmente iremos ter contato com as novas tecnologias!",
+                afirmacao: [
+                "Certa, porque finalmente tivemos acesso a tecnologia a uma nova realidade!",
+                "Certa, porque podemos aprender muitas coisas com a tecnologia!",
+                ]
+            }
+            
+            
+        ]
+    },
+    {
+        enunciado: "Como utilizar IA da forma correta para fazer um  trabalho gerando imagens? ",
+        alternativas: [
+            {
+                texto: "Utilizando de maneira correta, sem utilizar imagens prontas e criando a sua própria imagem!",
+                afirmacao: [
+                "Certa, porque a pessoa estaria realmente criando o seu projeto e aprendendo cada vez mais!",
+                "Certa, porque assim a pessoa estaria aprendendo como fazia o projeto e fazendo o seu trabalho da forma como queria!",
+                ]
+            },
+            {
+                texto: "Roubando dados de outras pessoas que já haviam feito antes!",
+                afirmacao: [
+                "Errada, porque assim a pessoa estaria pegando um projeto pronto e não criando um!",
+                "Errada, porque o correto é fazer seu próprio trabalho para aprender como realmente ele funciona!",
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Qual a importância da inteligência artificial nos dias de hoje?",
+        alternativas: [
+            {
+                texto: "Não tem nenhuma importância, visando que só trouxe complicações para a sociedade!",
+                afirmacao: [
+                "Errado, porque ela é de suma importância para a sociedade!",
+                "Errado, porque a inteligência artificial é muito importante no atual modo de vida que vivemos hoje!",
+                ]
+            },
+            {
+                texto: "Ela ajuda a resolver problemas mais complexos que os seres humanos não seriam capazes de resolver, e o melhoramento na área da saúde, educação e transportes!",
+                afirmacao: [
+                "Certa, porque ela é muito importante e com o seu avanço ela trouxe muitos benefícios!",
+                "Certa, porque ela transformou a vida de algumas pessoas de uma forma que foi muito importante para o avanço os dias de hoje, trazendo muitas expectativas o que antes da inteligência artificial não tinha!",
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Porque a Inteligência Artificial é muito utilizada nas escolas?",
+        alternativas: [
+            {
+                texto: "Para reduzir os custos de ensino, e ajudar nos processos de avaliações!",
+                afirmacao: [
+                "Certo, porque diminui os gastos com materiais e a tecnologia ajuda muito no processo de correção das avaliações!",
+                "Certo, porque se torna muito mais fácil fazer trabalhos ou explicar algum tema através da interne!",
+                ]
+
+            },
+            {
+                texto: "Porque os alunos não tem acesso a livros manuais!",
+                afirmacao: [
+                "Errada, porque todos os alunos tem acesso a livros!",
+                "Porque nem todos os alunos tem acesso à tecnologia, o que torna muito mais fácil utilizar livros manuais1",
+                ]
+            }
+        ]
+    }
+];
